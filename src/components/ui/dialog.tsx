bb76ddde9d751a5e410edb364c33a7c1fa96fab1
yizhe-ang <ang.yizhe@u.nus.edu>
2026-05-20 17:21:17 +0200

    update

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -47,7 +47,7 @@ function DialogContent({
 }) {
   return (
     <DialogPortal>
-      <DialogOverlay />
+      {/* <DialogOverlay /> */}
       <DialogPrimitive.Popup
         data-slot="dialog-content"
         className={cn(
