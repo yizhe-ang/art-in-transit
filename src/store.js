cf407faaef03360a9485e77af03330bb3009d625
yizhe-ang <ang.yizhe@u.nus.edu>
2026-05-20 16:52:08 +0200

    update

--- src/store.js
+++ src/store.js
@@ -3,4 +3,7 @@ import { create } from "zustand"
 export const useStore = create((set) => ({
   openArtworkDialog: true,
   setOpenArtworkDialog: (openArtworkDialog) => set({ openArtworkDialog }),
+
+  selectedArtwork: null,
+  setSelectedArtwork: (selectedArtwork) => set({ selectedArtwork }),
 }))
