3645d73a701f43464db447622716eea0d940e4cf
yizhe-ang <ang.yizhe@u.nus.edu>
2026-05-20 15:32:36 +0200

    update

--- src/components/three/lines.jsx
+++ src/components/three/lines.jsx
@@ -1,28 +1,94 @@
 import { useEffect, useMemo } from "react"
 import { buildRailRoutes } from "@/components/three/rail-routes"
+import { useLoader } from "@react-three/fiber"
 import { folder, useControls } from "leva"
-import { float, select, uniform, uv } from "three/tsl"
+import * as THREE from "three/webgpu"
+import { float, max, select, texture, uniform, uv, vec2 } from "three/tsl"
 
-// TODO: These should have a pencil / brush texture
+// TODO: Brush repeat could be fun to animate
+
+const BRUSH_REPEAT_LENGTH = 2_400
+const MIN_BRUSH_REPEATS = 1
 
 const drawTUniform = uniform(0)
+const brushRepeatLengthUniform = uniform(BRUSH_REPEAT_LENGTH)
+const strokeOpacityUniform = uniform(1)
+const alphaCutoffUniform = uniform(0.08)
 
-const lineNodes = {
-  opacityNode: select(uv().x.lessThanEqual(drawTUniform), float(1), float(0)),
-  alphaTestNode: float(0.5),
+function configureStrokeTexture(strokeTexture) {
+  strokeTexture.wrapS = THREE.RepeatWrapping
+  strokeTexture.wrapT = THREE.ClampToEdgeWrapping
+  strokeTexture.minFilter = THREE.LinearMipmapLinearFilter
+  strokeTexture.magFilter = THREE.LinearFilter
+  strokeTexture.colorSpace = THREE.NoColorSpace
+  strokeTexture.needsUpdate = true
+
+  return strokeTexture
+}
+
+function createLineNodes(strokeTexture, routeLength) {
+  const brushRepeats = max(
+    float(MIN_BRUSH_REPEATS),
+    float(routeLength).div(brushRepeatLengthUniform)
+  )
+  const brushUv = vec2(uv().x.mul(brushRepeats), uv().y)
+  const brushAlpha = texture(strokeTexture, brushUv).a
+  const drawMask = select(uv().x.lessThanEqual(drawTUniform), float(1), float(0))
+
+  return {
+    opacityNode: drawMask.mul(brushAlpha).mul(strokeOpacityUniform),
+    alphaTestNode: alphaCutoffUniform,
+  }
+}
+
+const PainterlyRoute = ({ route, strokeTexture }) => {
+  const lineNodes = useMemo(() => {
+    return createLineNodes(strokeTexture, route.length)
+  }, [route.length, strokeTexture])
+
+  return (
+    <mesh>
+      <tubeGeometry args={[route.curve, route.segments, 35, 8, false]} />
+      <meshBasicNodeMaterial color={route.color} transparent {...lineNodes} />
+    </mesh>
+  )
 }
 
 const Lines = () => {
+  const strokeTexture = useLoader(THREE.TextureLoader, "/textures/stroke.png")
+
+  useEffect(() => {
+    configureStrokeTexture(strokeTexture)
+  }, [strokeTexture])
+
   const routes = useMemo(() => {
     return buildRailRoutes()
   }, [])
 
-  const { drawT } = useControls({
+  const { alphaCutoff, brushRepeatLength, drawT, strokeOpacity } = useControls({
     lines: folder({
       drawT: {
-        value: 0,
+        value: 1,
         min: 0,
         max: 1,
+        step: 0.01,
+      },
+      brushRepeatLength: {
+        value: BRUSH_REPEAT_LENGTH,
+        min: 600,
+        max: 6_000,
+        step: 100,
+      },
+      strokeOpacity: {
+        value: 1,
+        min: 0,
+        max: 1,
+        step: 0.01,
+      },
+      alphaCutoff: {
+        value: 0.08,
+        min: 0,
+        max: 0.4,
         step: 0.01,
       },
     }),
@@ -32,18 +98,21 @@ const Lines = () => {
     drawTUniform.value = drawT
   }, [drawT])
 
+  useEffect(() => {
+    alphaCutoffUniform.value = alphaCutoff
+    brushRepeatLengthUniform.value = brushRepeatLength
+    strokeOpacityUniform.value = strokeOpacity
+  }, [alphaCutoff, brushRepeatLength, strokeOpacity])
+
   return (
     <>
       <group>
         {routes.map((route) => (
-          <mesh key={route.id}>
-            <tubeGeometry args={[route.curve, route.segments, 35, 6, false]} />
-            <meshBasicNodeMaterial
-              color={route.color}
-              transparent
-              {...lineNodes}
-            />
-          </mesh>
+          <PainterlyRoute
+            key={route.id}
+            route={route}
+            strokeTexture={strokeTexture}
+          />
         ))}
       </group>
     </>
